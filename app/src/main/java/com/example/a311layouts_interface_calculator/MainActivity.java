6276da86b629cc7a1f90b9f93cda9441f547a975
denlog1977 <denlog1977@gmail.com>
2019-10-28 22:32:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a311layouts_interface_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a311layouts_interface_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
